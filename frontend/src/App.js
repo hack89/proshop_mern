5b29ad3e6c772493033a829fcf2d0626bcbc1f9f
hack89 <hackinsta89@gmail.com>
2020-11-15 12:55:12 +0100

    add_backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
 
 const App = () => (
-  <>
-    <h1>Welcome</h1>
-  </>
+  <Router>
+    <Header />
+    <main className="py-3">
+      <Container>
+        <Route exact path="/" component={HomeScreen} />
+        <Route path="/product/:id" component={ProductScreen} />
+        <Route path="/cart/:id?" component={CartScreen} />
+        {/* il ? indica che è opzionale */}
+      </Container>
+    </main>
+    <Footer />
+  </Router>
 );
 
 export default App;
